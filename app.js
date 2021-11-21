3454e7c0950549b3e7dcf438d60156d6aa43c993
Gabriel Ortega <gabrielo_9103@hotmail.com>
2021-11-21 11:15:21 -0500

    tests fixing

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const _ = require('lodash');
+const axios = require('axios');
+const { escapeRegExp } = require('lodash');
 const app = express();
 
 app.get('/api/health-check', (req, res) => {
   return res.send({ status: 'ok' });
+});
+
+app.get('/api/pokemon', async (req, res) => {
+  const { filter } = req.query;
+  const result = await axios.get('https://pokeapi.co/api/v2/pokemon');
+
+  if (filter) {
+    const {
+      data: { results },
+    } = result;
+
+    const filteredData = _.filter(results, ({ name }) => name.match(filter));
+    return res.send(filteredData);
+  }
+
+  return res.send(result.data);
 });
 
 module.exports = app;
--- index.test.js
+++ index.test.js
@@ -1,8 +1,22 @@
 const supertest = require('supertest');
+const mockResult = require('./mocks/pokemon.json');
+const nock = require('nock');
 
 const app = require('./app');
 
 it('Get health check', async () => {
   const res = await supertest(app).get('/api/health-check');
   expect(res.status).toEqual(200);
+});
+
+it('Get pokemon', async () => {
+  const res = await supertest(app).get('/api/pokemon');
+  expect(res.body).toMatchObject({ count: 1118 });
+});
+
+it('Get pokemon filtered', async () => {
+  nock('https://pokeapi.co:443').get('/api/v2/pokemon').reply(200, mockResult);
+
+  const res = await supertest(app).get('/api/pokemon?filter=saur');
+  expect(res.body).toHaveLength(3);
 });
