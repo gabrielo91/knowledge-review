b222a2eeef302b403a17c510b5e4e633481c5ef9
Gabriel Ortega <gabrielo_9103@hotmail.com>
2021-11-21 11:34:45 -0500

    spies added

--- index.test.js
+++ index.test.js
@@ -1,22 +1,37 @@
+const nock = require('nock');
+const sinon = require('sinon');
 const supertest = require('supertest');
 const mockResult = require('./mocks/pokemon.json');
-const nock = require('nock');
+const _ = require('lodash');
 
 const app = require('./app');
+describe('Test index.js', () => {
+  const sandbox = sinon.createSandbox();
 
-it('Get health check', async () => {
-  const res = await supertest(app).get('/api/health-check');
-  expect(res.status).toEqual(200);
-});
+  afterEach(() => {
+    sandbox.restore();
+  });
 
-it('Get pokemon', async () => {
-  const res = await supertest(app).get('/api/pokemon');
-  expect(res.body).toMatchObject({ count: 1118 });
-});
+  it('Get health check', async () => {
+    const res = await supertest(app).get('/api/health-check');
+    expect(res.status).toEqual(200);
+  });
 
-it('Get pokemon filtered', async () => {
-  nock('https://pokeapi.co:443').get('/api/v2/pokemon').reply(200, mockResult);
+  it('Get pokemon', async () => {
+    const res = await supertest(app).get('/api/pokemon');
+    var spy = sandbox.spy(_, 'filter');
+    expect(spy.calledOnce).toBeFalsy();
+    expect(res.body).toMatchObject({ count: 1118 });
+  });
 
-  const res = await supertest(app).get('/api/pokemon?filter=saur');
-  expect(res.body).toHaveLength(3);
+  it('Get pokemon filtered', async () => {
+    nock('https://pokeapi.co:443')
+      .get('/api/v2/pokemon')
+      .reply(200, mockResult);
+
+    var spy = sandbox.spy(_, 'filter');
+    const res = await supertest(app).get('/api/pokemon?filter=saur');
+    expect(spy.calledOnce).toBeTruthy();
+    expect(res.body).toHaveLength(3);
+  });
 });
